3e156c494fe66314bb45a6c86608d921ea0e9bbc
Nikudo-4 <kunach1lo@gmail.com>
2023-03-09 17:40:34 +0300

    first commit

---
+++ src/Telegram.php
@@ -0,0 +1,21 @@
+<?php
+
+class Telegram 
+{
+    public function request($data, $method)
+    {
+        $token = "6213674319:AAGDoShgj7fKICIRml9wDdwmxw0vJ5jyFso";
+        $ch = curl_init("https://api.telegram.org/bot{$token}/{$method}");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data, JSON_UNESCAPED_UNICODE));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        $res = curl_exec($ch);
+        curl_close($ch);
+          
+        $res = json_encode($res, JSON_UNESCAPED_UNICODE);
+        print_r($res);
+    }
+}
